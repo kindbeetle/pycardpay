c804ccd91c5d0ae8e29abcf5be81c662a303f839
k.pavlov <k.pavlov@cardpay.com>
2014-05-02 14:16:07 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='com.cardpay.api',
+    version='1.0.0',
+    packages=['PyCardPay'],
+    url='https://github.com/cardpay/python-api',
+    license='',
+    author='a.maslov',
+    author_email='',
+    description='Python API for CardPay Payment Service'
+)

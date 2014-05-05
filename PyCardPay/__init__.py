df898bf7ac705e0052e14c78d0a658ef2abb6d8d
Maestro <maestro@Maestro-Laptop.local>
2014-05-05 05:54:29 +0300

    remove non-docs from gh-pages branch

--- PyCardPay/__init__.py
+++
@@ -1,2 +0,0 @@
-from .api import capture, pay, refund, status, status_change, void, finish_3ds
-from .utils import order_to_xml, xml_to_string, xml_get_sha512, xml_check_sha512
--- PyCardPay/settings.py
+++
@@ -1,4 +0,0 @@
-## Please, contact support@cardpay.com to get URLs
-url_status_change = ''
-url_status = ''
-url_pay = ''
